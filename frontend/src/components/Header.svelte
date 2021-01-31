f31821ce7ca5fb14bea6ecdc77ebede36f947a5e
Nate Sales <nate@natesales.net>
2021-01-31 01:45:41 -0800

    add github link

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -5,9 +5,7 @@
     HeaderUtilities,
     HeaderGlobalAction,
   } from "carbon-components-svelte";
-  import Notification20 from "carbon-icons-svelte/lib/Notification20";
-  import UserAvatar20 from "carbon-icons-svelte/lib/UserAvatar20";
-  import AppSwitcher20 from "carbon-icons-svelte/lib/AppSwitcher20";
+  import LogoGithub20 from "carbon-icons-svelte/lib/LogoGithub20";
   import { getContext } from "svelte";
 
   const ctx = getContext("Theme");
@@ -23,13 +21,11 @@
   }
 </script>
 
-<Header company="IBM" platformName="Carbon Components Svelte" href="/">
+<Header company="IRR GUI" platformName="Responsive web UI for IRR object management" href="/">
   <div slot="skip-to-content">
     <SkipToContent />
   </div>
   <HeaderUtilities>
-    <HeaderGlobalAction aria-label="Notifications" icon="{Notification20}" />
-    <HeaderGlobalAction aria-label="User Avatar" icon="{UserAvatar20}" />
-    <HeaderGlobalAction aria-label="App Switcher" icon="{AppSwitcher20}" />
+    <HeaderGlobalAction aria-label="GitHub Repo" icon="{LogoGithub20}" on:click={() => window.location = "https://github.com/natesales/irr-gui"}/>
   </HeaderUtilities>
 </Header>
