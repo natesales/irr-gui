a5c92bbfb086aece2723fcc8629cd462f7c06182
Nate Sales <nate@natesales.net>
2021-01-31 00:47:06 -0800

    add loading during backend query

--- frontend/src/components/ObjectTable.svelte
+++ frontend/src/components/ObjectTable.svelte
@@ -1,9 +1,8 @@
 <script>
-    import {Button, DataTable, Toolbar, ToolbarContent, Link} from "carbon-components-svelte";
-    import Launch16 from "carbon-icons-svelte/lib/Launch16";
+    import {Button, DataTable, Toolbar, ToolbarContent, Loading} from "carbon-components-svelte";
     import {onMount} from "svelte";
 
-    let objects = [];
+    let objects;
 
     onMount(() => {
         fetch("https://localhost/api/query", {
@@ -16,44 +15,46 @@
             .then(resp => resp.json())
             .then(data => {
                 objects = data
-                console.log(objects)
             })
     })
 </script>
 
 <main>
-    <DataTable
-            headers={[
-                { key: "custom-primary-key", value: "Primary Key" },
-                { key: "custom-object-type", value: "Type" },
-                { key: "descr", value: "Description"},
-                { key: "custom-maintainers", value: "Maintainers"},
-                { key: "source", value: "Source"}
-            ]}
-            rows={objects}
-            sortable
-    >
-<!--        <span slot="cell" let:row let:cell>-->
-<!--            {#if cell.key === 'source' && cell.value === 'ARIN'}-->
-<!--              <Link inline href="https://en.wikipedia.org/wiki/Round-robin_DNS" target="_blank"-->
-<!--              >-->
-<!--                {cell.value}-->
-<!--                  <Launch16 />-->
-<!--              </Link>-->
-<!--            {:else}{cell.value}{/if}-->
-<!--        </span>-->
+    {#if objects}
+        <DataTable
+                headers={[
+                    { key: "custom-primary-key", value: "Primary Key" },
+                    { key: "custom-object-type", value: "Type" },
+                    { key: "descr", value: "Description"},
+                    { key: "custom-maintainers", value: "Maintainers"},
+                    { key: "source", value: "Source"}
+                ]}
+                rows={objects}
+                sortable
+        >
 
-        <Toolbar>
-            <ToolbarContent>
-                <!--                TODO: Implement search -->
-                <!--                <ToolbarSearch/>-->
-                <!--                <ToolbarMenu>-->
-                <!--                    <ToolbarMenuItem primaryFocus>Restart all</ToolbarMenuItem>-->
-                <!--                    <ToolbarMenuItem href="https://cloud.ibm.com/docs/loadbalancer-service">API documentation</ToolbarMenuItem>-->
-                <!--                    <ToolbarMenuItem danger>Stop all</ToolbarMenuItem>-->
-                <!--                </ToolbarMenu>-->
-                <Button>Create object</Button>
-            </ToolbarContent>
-        </Toolbar>
-    </DataTable>
+            <span slot="cell" let:row let:cell>
+                {#if cell.value === undefined}
+                    None
+                {:else}
+                    {cell.value}
+                {/if}
+            </span>
+
+            <Toolbar>
+                <ToolbarContent>
+                    <!--                TODO: Implement search -->
+                    <!--                <ToolbarSearch/>-->
+                    <!--                <ToolbarMenu>-->
+                    <!--                    <ToolbarMenuItem primaryFocus>Restart all</ToolbarMenuItem>-->
+                    <!--                    <ToolbarMenuItem href="https://cloud.ibm.com/docs/loadbalancer-service">API documentation</ToolbarMenuItem>-->
+                    <!--                    <ToolbarMenuItem danger>Stop all</ToolbarMenuItem>-->
+                    <!--                </ToolbarMenu>-->
+                    <Button>Create object</Button>
+                </ToolbarContent>
+            </Toolbar>
+        </DataTable>
+    {:else}
+        <Loading/>
+    {/if}
 </main>
